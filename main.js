768da94932d7e4845173208ae35988d02bdedc60
Vadims Malickis <vadims.malickis@rvt.lv>
2022-12-13 12:45:35 +0200

    moved git-repo functionality to another repo

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-fetch("http://localhost:8080/git-repos.php")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    data.repos.forEach((element) => {
-      let container = document.createElement("div");
-      container.classList.add("repo-container");
-      container.textContent = element.id;
-      document.querySelector("main").appendChild(container);
-    });
-  });
-
 function populateDocument(fromFile, targetElement) {
   fetch(fromFile)
     .then((response) => {
