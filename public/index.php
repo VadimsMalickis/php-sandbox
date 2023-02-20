7a13d1bc473c2969efc4e1791ee07c39199d04a7
Vadims Malickis <117085106+VadimsMalickis@users.noreply.github.com>
2023-02-20 09:24:50 +0000

    refactor file structure

--- public/index.php
+++ public/index.php
@@ -6,13 +6,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-require_once __DIR__ .  "/vendor/autoload.php";
+require_once __DIR__ .  "/../vendor/autoload.php";
 
-$routes = include __DIR__.'/routes.php';
+$routes = include __DIR__ . '/../routes.php';
 $request = Request::createFromGlobals();
 
 $app = new Application($routes);
-$twig = new Environment(new FilesystemLoader(__DIR__.'/views'));
+$twig = new Environment(new FilesystemLoader(__DIR__ . '/../views'));
 Application::withTemplateEngine($twig);
 $response = $app->handleRequest($request);
 
--- src/Controller.php
+++ src/Controller.php
@@ -5,18 +5,25 @@ namespace App;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class Controller {
+class Controller
+{
 
     public function homeAction(Request $request): Response
     {
+        $productCategory = [
+            'Computers' => [],
+            'Furniture' => []
+        ];
+
+
         return Application::render('home.html');
     }
 
-    public function contactAction(Request $request, $username): Response {
+    public function contactAction(Request $request, $username): Response
+    {
 
         return Application::render('contact.html', [
             'username' => $username
         ]);
     }
-
 }
