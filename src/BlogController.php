23a180f490340ff9d5b6b88a3d710a62c83f0cf9
VadimsMalickis <vadims.malickis@rvt.lv>
2023-05-04 09:09:19 +0300

    update

--- src/BlogController.php
+++ src/BlogController.php
@@ -11,7 +11,37 @@ class BlogController extends AbstractController
 {
     public function indexAction(): Response
     {
+        $dob = '19-07-1990';
+        $errors = [];
+
+        try {
+            // new DateTime will throw an error if user date was corrupt
+            $dob = new \DateTime($dob);
+        } catch (\Exception $e) {
+            // errors[] = 'something' is same as array_push(...)
+            $errors[] = 'Invalid date!';
+            exit();
+        }
+
+        $minInterval = \DateInterval::createFromDateString('18 years');
+        $maxInterval = \DateInterval::createFromDateString('130 years');
+
+
+        $minDobLimit = ( new \DateTime() )->sub($minInterval);
+        $maxDobLimit = ( new \DateTime() )->sub($maxInterval);
+
+        if ($dob <= $maxDobLimit) {
+            $errors[] = 'You must be alive to use this service.';
+            exit();
+        }
+        if ($dob >= $minDobLimit) {
+            $errors[] = 'Not 18+ years!';
+            exit();
+        }
+
         return $this->render('blog.html');
     }
+
+
 
 }
